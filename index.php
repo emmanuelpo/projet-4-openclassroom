fa039a460db10bb6f2cd182baa2ca6c96da09541
Pixrr <pierry2310@gmail.com>
2019-05-05 09:20:18 +0200

    ♻️ refactor(index.php): flat conditions in file

--- index.php
+++ index.php
@@ -1,37 +1,53 @@
 <?php
+
 require('controller/Chapter_frontend.php');
 require('controller/Comment_frontend.php');
-try{
-if (isset($_GET['action'])) {
-	if ($_GET['action'] == 'listChapter'){
-		listChapter();
-	}
-	elseif ($_GET['action'] == 'listComment'){
-		if (isset($_GET['id']) && $_GET['id'] > 0) {
-			listComment();
-		}
-		else{
-			throw new Exception('Erreur: aucun identifiant de chapitre envoyé');
-		}
-	}
-	elseif ($_GET['action'] == 'addComment') {
-		if (isset($_GET['id']) && $_GET['id'] > 0){
-			if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-				addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-			}
-			else {
-				echo 'Erreur: tout les champs ne sont pas remplis !';
-				}
-			}
-			else{
-				echo 'Erreur! aucun identifiant de chapitre envoyé';
-			}
-		}
-	}
-	else{
-		listChapter();
-	}
+
+function matchWithAction()
+{
+    /**
+     * For switch usage
+     * @see https://www.php.net/manual/en/control-structures.switch.php
+     */
+    switch ($_GET['action']) {
+        case 'addComment':
+            if (!isset($_GET['id'])) {
+                throw new Exception('Erreur! aucun identifiant de chapitre envoyé');
+            }
+            if ($_GET['id'] < 1) { // instead of 0
+                throw new Exception('Erreur! aucun identifiant de chapitre envoyé');
+            }
+            if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+            }
+
+            throw new Exception('Erreur: tout les champs ne sont pas remplis !');
+            break;
+        case 'listChapter':
+            listChapter();
+            break;
+        /**
+         * See view/listChapters.php line 22
+         */
+        case 'post': // instead of 'listComment'
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                listComment();
+            }
+
+            throw new Exception('Erreur: aucun identifiant de chapitre envoyé');
+            break;
+        default:
+            listChapter();
+            break;
+    }
 }
-catch(Exception $e) { // S'il y a eu une erreur, alors...
-    echo 'Erreur : ';
+
+try {
+    matchWithAction();
+} catch (Throwable $e) { // S'il y a eu une erreur, alors...
+    /**
+     * For sprintf usage
+     * @see https://www.php.net/manual/en/function.sprintf.php
+     */
+    echo sprintf('%s: %s', 'Erreur', $e->getMessage());
 }
